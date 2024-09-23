96d5867b96ec9db6a05c826f16217491fa769891
Ben-levi <131279096+Ben-levi@users.noreply.github.com>
2024-09-23 23:09:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 current_datetime = datetime.now()
-print("Current date and time:", current_datetime)
+day_of_week = current_datetime.strftime("%A")
 
-#####new push on git
-print("new_push")
+print(f"Current date and time: {current_datetime}")
+print(f"Day of the week: {day_of_week}")
